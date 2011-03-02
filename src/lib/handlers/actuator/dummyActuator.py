a35d7b95d52a6ca863d09f9ba766b1227298d468
Cameron Finucane <cpf37@cornell.edu>
2011-03-02 02:59:52 -0500

    Shortened delay on dummyActuator

--- src/lib/handlers/actuator/dummyActuator.py
+++ src/lib/handlers/actuator/dummyActuator.py
@@ -19,7 +19,7 @@ class actuatorHandler:
         """
 
         if val:
-            time.sleep(0.5)  # Fake some time lag for the actuator to enable
+            time.sleep(0.1)  # Fake some time lag for the actuator to enable
 
         print "(ACT) Actuator %s is now %s!" % tuple(map(str, (name, val)))
 
