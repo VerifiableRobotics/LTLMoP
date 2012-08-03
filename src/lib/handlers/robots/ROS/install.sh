2d9a1e2738ea598499581175aa901577ef732f1a
Stephen Smith <smsmit18@ncsu.edu>
2012-08-03 13:29:33 -0400

    updates to install script

--- src/lib/handlers/robots/ROS/install.sh
+++ src/lib/handlers/robots/ROS/install.sh
@@ -1,30 +1,37 @@
 #! /bin/bash
 echo Installing ROS...
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros/latest.list'
+sudo apt-get install python-wxglade
+wget https://github.com/downloads/jraedler/Polygon2/Polygon-2.0.5.zip
+unzip Polygon-2.0.5.zip
+cd Polygon-2.0.5
+sudo python setup.py install
+cd ..
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install ros-fuerte-desktop-full
 echo "source /opt/ros/fuerte/setup.bash" >> ~/.bashrc
 . ~/.bashrc
-source /opt/ros/furete/setup.bash
+source /opt/ros/fuerte/setup.bash
 sudo apt-get install python-setuptools python-pip
 sudo pip install -U rosinstall vcstools rosdep
 sudo easy_install -U rosinstall vcstools rosdep
 sudo apt-get install ros-fuerte-pr2-desktop
 echo "source /opt/ros/fuerte/setup.bash" >> ~/.bashrc
 . ~/.bashrc
-source /opt/ros/furete/setup.bash
+source /opt/ros/fuerte/setup.bash
 sudo apt-get install python-rsvg
-PATHSM='/opt/ros/fuerte/stacks/simulator_gazebo/gazebo_worlds/'
+PATHSM='/opt/ros/fuerte/stacks/simulator_gazebo/gazebo_worlds'
 sudo cp ltlmop_map.world $PATHSM/worlds/ltlmop_map.world
 sudo chmod 777 $PATHSM/worlds/ltlmop_map.world
 sudo chmod 777 $PATHSM/worlds
 sudo chmod 777 $PATHSM/launch
 sudo cp ltlmop.launch $PATHSM/launch/ltlmop.launch
 sudo chmod 777 $PATHSM/launch/ltlmop.launch
-PATHLOC='/opt/ros/fuerte/stacks/simulator_gazebo/gazebo/gazebo/share/gazebo-1.0.2/Media/materials/'
+PATHLOC='/opt/ros/fuerte/stacks/simulator_gazebo/gazebo/gazebo/share/gazebo-1.0.2/Media/materials'
 sudo chmod 777 /opt/ros/fuerte/stacks/simulator_gazebo/gazebo/gazebo/share/gazebo-1.0.2/Media/materials/textures
 if ! grep -q ltlmop $PATHLOC/scripts/Gazebo.material 
 	then sudo cat ltlmop.material >> $PATHLOC/scripts/Gazebo.material 
 fi
 sudo chmod 777 $PATHLOC/scripts/Gazebo.material
+ROS_WORKSPACE=/opt/ros/fuerte
