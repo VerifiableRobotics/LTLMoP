525cbbbdda46bea934b31229e709048bc44e5ded
Cameron Finucane <cpf37@cornell.edu>
2012-10-04 22:31:45 -0400

    add 'region' type to dummysensor; update followtest spec to use it

--- src/examples/followtest/followtest.spec
+++ src/examples/followtest/followtest.spec
@@ -40,23 +40,44 @@ kitchen = p5
 
 Spec: # Specification in structured English
 
-# env assumptions
+# === env assumptions ===
+# basically, to keep the automaton smaller,
+# we assume that the thing we are following will follow
+# the topology of the map the same way we must,
+# and shall not skip between rooms
+
+# this part is auto-generated because it is messy
 
 ENVIRONMENT TOPOLOGY
 
-#things are OK if we force the env to always stay in r6
-#(!e.sbit0 & !e.sbit1 & !e.sbit2 & !e.sbit3 & !e.sbit4)
-#[](!next(e.sbit0) & !next(e.sbit1) & !next(e.sbit2) & !next(e.sbit3) & !next(e.sbit4))
 --
 
-# sys reqs
+# === sys reqs ===
+# start in the porch
 porch
-#[](next(s.env_stationary) = ((e.sbit0 = next(e.sbit0)) & (e.sbit1 = next(e.sbit1)) & (e.sbit2 = next(e.sbit2)) & (e.sbit3 = next(e.sbit3)) & (e.sbit4 = next(e.sbit4))))
+
+# keep track of whether the target moved during its last "turn" or not
 [](next(s.env_stationary) = ((e.sbit0 = next(e.sbit0)) & (e.sbit1 = next(e.sbit1)) & (e.sbit2 = next(e.sbit2))))
 
-#[]<>(s.env_stationary -> ((s.bit0 = e.sbit0) & (s.bit1 = e.sbit1) & (s.bit2 = e.sbit2) & (s.bit3 = e.sbit3) & (s.bit4 = e.sbit4)))
-[]<>(s.env_stationary -> ((s.bit0 = e.sbit0) & (s.bit1 = e.sbit1) & (s.bit2 = e.sbit2)))
+# always end up in the same place as the target,
+# although weakened to let us stay in place if
+# the target is constantly moving (it is not clear
+# what "follow" would even mean in this scenario, for that matter)
 
-# beep if env is in r6
-#[](next(s.beep) <-> (!next(e.sbit0) & !next(e.sbit1) & !next(e.sbit2) & !next(e.sbit3) & !next(e.sbit4)))
+# single-goal style: []<>(s.env_stationary -> ((s.bit0 = e.sbit0) & (s.bit1 = e.sbit1) & (s.bit2 = e.sbit2)))
+
+# multi-goal style:
+[]<>((s.env_stationary & (!e.sbit0 & !e.sbit1 & !e.sbit2)) -> (!s.bit0 & !s.bit1 & !s.bit2))
+[]<>((s.env_stationary & (!e.sbit0 & !e.sbit1 & e.sbit2)) -> (!s.bit0 & !s.bit1 & s.bit2))
+[]<>((s.env_stationary & (!e.sbit0 & e.sbit1 & !e.sbit2)) -> (!s.bit0 & s.bit1 & !s.bit2))
+[]<>((s.env_stationary & (!e.sbit0 & e.sbit1 & e.sbit2)) -> (!s.bit0 & s.bit1 & s.bit2))
+[]<>((s.env_stationary & (e.sbit0 & !e.sbit1 & !e.sbit2)) -> (s.bit0 & !s.bit1 & !s.bit2))
+[]<>((s.env_stationary & (e.sbit0 & !e.sbit1 & e.sbit2)) -> (s.bit0 & !s.bit1 & s.bit2))
+[]<>((s.env_stationary & (e.sbit0 & e.sbit1 & !e.sbit2)) -> (s.bit0 & s.bit1 & !s.bit2))
+[]<>((s.env_stationary & (e.sbit0 & e.sbit1 & e.sbit2)) -> (s.bit0 & s.bit1 & s.bit2))
+
+
+# UNREALIZABILITY TEST:
+# never go in the bedroom!
+[](!bedroom)
 
--- src/lib/handlers/share/_SensorHandler.py
+++ src/lib/handlers/share/_SensorHandler.py
@@ -5,6 +5,9 @@
 import wx, wx.lib.buttons
 import sys, socket
 import wx.lib.delayedresult as delayedresult
+sys.path.append("lib")
+import project
+import mapRenderer
 
 # begin wxGlade: extracode
 # end wxGlade
@@ -27,6 +30,11 @@ class SensorEditorFrame(wx.Frame):
 
         self.buttons = [] # This will later hold our buttons
 
+        self.map_panels = {} # This will later hold our map panels
+        self.map_bitmaps = {} # This will later hold our map panel bitmaps
+        self.map_state = {} # This will later hold current region states
+        self.map_scales = {}
+
         self.waitingForInput = False
         self.Bind(wx.EVT_IDLE, self.onIdle)
         
@@ -38,8 +46,8 @@ class SensorEditorFrame(wx.Frame):
         self.UDPSock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
         # Let everyone know we're ready
         #print "Hello!"
-        self.UDPSock.sendto("Hello!",self.addr)
-        self.UDPSock.close()        
+        self.UDPSock.sendto("Hello!\n",self.addr)
+        #self.UDPSock.close()        
 
     def __set_properties(self):
         # begin wxGlade: SensorEditorFrame.__set_properties
@@ -60,12 +68,6 @@ class SensorEditorFrame(wx.Frame):
 
     def sensorToggle(self, event): # wxGlade: SensorEditorFrame.<event_handler>
         btn = event.GetEventObject()
-        self.host = 'localhost'
-        self.port = 23459
-        self.buf = 1024
-        self.addr = (self.host,self.port)
-        self.UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        self.UDPSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         self.UDPSock.sendto(btn.GetLabelText() + "=" + str(btn.GetValue()),self.addr)
         #print btn.GetLabelText() + "=" + str(btn.GetValue())
@@ -91,28 +93,47 @@ class SensorEditorFrame(wx.Frame):
 
         try: 
             # Read in the information
-            [sensor_name, sensor_value] = line.split(",")
+            [sensor_type, sensor_name, sensor_value] = line.split(",")
             
-            # Create the new button and add it to the sizer
-            self.buttons.append(wx.lib.buttons.GenToggleButton(self.panel_1, -1, sensor_name))
-            big_font = wx.Font(pointSize=24, family=wx.FONTFAMILY_DEFAULT, style=wx.FONTSTYLE_NORMAL, weight=wx.FONTWEIGHT_NORMAL)
-            self.buttons[-1].SetFont(big_font)
-            self.panel_1.GetSizer().Add(self.buttons[-1], 1, wx.EXPAND, 0)
+            if sensor_type.strip().lower() == "button":
+                # Create the new button and add it to the sizer
+                self.buttons.append(wx.lib.buttons.GenToggleButton(self.panel_1, -1, sensor_name))
+                big_font = wx.Font(pointSize=24, family=wx.FONTFAMILY_DEFAULT, style=wx.FONTSTYLE_NORMAL, weight=wx.FONTWEIGHT_NORMAL)
+                self.buttons[-1].SetFont(big_font)
+                self.panel_1.GetSizer().Add(self.buttons[-1], 1, wx.EXPAND, 0)
 
-            # Set the initial value as appropriate
-            if sensor_value == "1":
-                self.buttons[-1].SetValue(True)
-                self.buttons[-1].SetBackgroundColour(wx.Colour(0, 255, 0)) 
-            else:
-                self.buttons[-1].SetValue(False)
-                self.buttons[-1].SetBackgroundColour(wx.Colour(255, 0, 0)) 
+                # Set the initial value as appropriate
+                if sensor_value == "1":
+                    self.buttons[-1].SetValue(True)
+                    self.buttons[-1].SetBackgroundColour(wx.Colour(0, 255, 0)) 
+                else:
+                    self.buttons[-1].SetValue(False)
+                    self.buttons[-1].SetBackgroundColour(wx.Colour(255, 0, 0)) 
+
+                # Bind to event handler
+                self.Bind(wx.EVT_BUTTON, self.sensorToggle, self.buttons[-1])
+            elif sensor_type.strip().lower() == "region":
+                if self.proj is None:
+                    print "Error: region sensor cannot be created without a project loaded"
+                else:
+                    # initialize
+                    self.map_state[sensor_name] = sensor_value
+
+                    self.map_panels[sensor_name] = wx.Panel(self.panel_1)
+
+                    self.Bind(wx.EVT_SIZE, self.onResize)
+                    self.panel_1.GetSizer().Add(self.map_panels[sensor_name], 1, wx.EXPAND, 0)
+
+                    self.map_panels[sensor_name].SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
+                    self.map_panels[sensor_name].Bind(wx.EVT_PAINT, self.onMapPaint)
+                    self.map_panels[sensor_name].Bind(wx.EVT_LEFT_DOWN, self.onMapClick)
+            elif sensor_type.strip().lower() == "loadproj":
+                self.proj =  project.Project()
+                self.proj.loadProject(sensor_name)
+                self.decomposedRFI = self.proj.loadRegionFile(decomposed=True)
 
             self.panel_1.Layout() # Update the frame
-            self.Refresh()
-
-            # Bind to event handler
-            self.Bind(wx.EVT_BUTTON, self.sensorToggle, self.buttons[-1])
-
+            self.onResize()
         except ValueError:
             print "(SENSOR) WARNING: Unexpected message received!"
 
@@ -123,6 +144,64 @@ class SensorEditorFrame(wx.Frame):
         if not self.waitingForInput:
             delayedresult.startWorker(self.updateFromInput, self.checkForInput)
             self.waitingForInput = True
+        event.Skip()
+
+    def onMapClick(self, event):
+        panel = event.GetEventObject()
+        panel_key = None
+        for k,v in self.map_panels.iteritems():
+            if v == panel:
+                panel_key = k
+                break
+
+        x = event.GetX()/self.map_scales[panel_key]
+        y = event.GetY()/self.map_scales[panel_key]
+        for region in self.decomposedRFI.regions:
+            if region.objectContainsPoint(x, y):
+                if region.name != self.map_state[panel_key]:
+                    self.map_state[panel_key] = region.name
+                    self.UDPSock.sendto(panel_key + "=" + region.name, self.addr)
+                break
+
+        self.onResize() # Force map redraw
+        event.Skip()
+
+    def onResize(self, event=None):
+        for k, p in self.map_panels.iteritems():
+            size = p.GetSize()
+            self.map_bitmaps[k] = wx.EmptyBitmap(size.x, size.y)
+            self.map_scales[k] = mapRenderer.drawMap(self.map_bitmaps[k], self.decomposedRFI, scaleToFit=True, drawLabels=True, memory=True, highlightList=self.map_state[k])
+
+        self.Refresh()
+        self.Update()
+
+        if event is not None:
+            event.Skip()
+
+    def onMapPaint(self, event):
+        panel = event.GetEventObject()
+        panel_key = None
+        for k,v in self.map_panels.iteritems():
+            if v == panel:
+                panel_key = k
+                break
+
+        if panel_key not in self.map_bitmaps:
+            return
+
+        pdc = wx.AutoBufferedPaintDC(panel)
+        try:
+            dc = wx.GCDC(pdc)
+        except:
+            dc = pdc
+
+        dc.BeginDrawing()
+
+        # Draw background
+        dc.DrawBitmap(self.map_bitmaps[panel_key], 0, 0)
+
+        dc.EndDrawing()
+
         event.Skip()
 
 # end of class SensorEditorFrame
