14b7c7cce5e0774c994fcf26d24a52f9e820c860
Vasumathi Raman <vasumathi.raman@gmail.com>
2013-01-17 16:44:36 -0500

    overzealous...

--- src/lib/specCompiler.py
+++ src/lib/specCompiler.py
@@ -433,7 +433,7 @@ class SpecCompiler(object):
                     isTrans[clause] = 1                    
                 else:
                     isTrans[clause] = 0  
-            conjuncts = conjuncts.extend(newCs) 
+            conjuncts = conjuncts + newCs 
 
             
         
