9612add03847eaaa562ecdeec1d7150adece251b
Cameron Finucane <cpf37@cornell.edu>
2013-01-29 20:40:50 -0500

    fix import path in region2svg

--- src/etc/utils/region2svg.py
+++ src/etc/utils/region2svg.py
@@ -1,6 +1,8 @@
-import sys, re
+import sys, re, os
 import Polygon, Polygon.IO
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 import lib.regions as regions
+
 
 fn_in = sys.argv[1]
 fn_out = re.sub(r"\.regions$", ".svg", fn_in)
