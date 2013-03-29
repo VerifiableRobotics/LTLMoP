5c5688693d6f18afec2036a3e4aa2110bbffaf87
Cameron Finucane <cpf37@cornell.edu>
2013-03-28 23:51:55 -0400

    re-enable dot export that was commented out for some reason

--- src/specEditor.py
+++ src/specEditor.py
@@ -1279,10 +1279,10 @@ class SpecEditorFrame(wx.Frame):
         aut = fsa.Automaton(proj_copy)
 
         aut.loadFile(self.proj.getFilenamePrefix()+".aut", self.proj.enabled_sensors, self.proj.enabled_actuators, self.proj.all_customs)
-        #aut.writeDot(self.proj.getFilenamePrefix()+".dot")
+        aut.writeDot(self.proj.getFilenamePrefix()+".dot")
         
         
-    #def _exportSMVFile(self):              
+    def _exportSMVFile(self):              
         aut.writeSMV(self.proj.getFilenamePrefix()+"MC.smv")
         
         
