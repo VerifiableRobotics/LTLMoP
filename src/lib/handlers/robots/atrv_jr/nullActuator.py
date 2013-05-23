dfdf2a4493d75d913f7b0abe0a0f7a2cc18affc8
Cameron Finucane <cpf37@cornell.edu>
2013-05-23 15:35:15 -0400

    remove nullsensor/actuator

--- src/lib/handlers/robots/atrv_jr/nullActuator.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-class nullActuatorHandler:
-    def __init__(self, proj, shared_data):
-        pass
-
--- src/lib/handlers/robots/atrv_jr/nullSensor.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-class nullSensorHandler:
-    def __init__(self, proj, shared_data):
-        pass
-
