bb9b5c28d947371f05131af301b9667c27264658
Izzy <gisrael@seas.upenn.edu>
2013-08-22 14:40:08 -0400

    Add serverproxy to pragbotInit for communication with PragbotClient in SLURP

--- src/lib/handlers/robots/pragbot/pragbotInit.py
+++ src/lib/handlers/robots/pragbot/pragbotInit.py
@@ -1,16 +1,19 @@
 """Provides initialization for other Gumbo handlers."""
 
+import xmlrpclib
 
 class gumboInitHandler:
 
     NODE_NAME = "gumbo_controller"
+    PRAGBOT_LISTEN_PORT = 20003
 
     def __init__(self, proj):  # pylint: disable=W0613
-        pass
+        self.pragbot_proxy = xmlrpclib.ServerProxy(
+            "http://localhost:{}".format(self.PRAGBOT_LISTEN_PORT), allow_none=True)
 
     def getSharedData(self):
         """Return a dict of objects shared with other handlers."""
-        return {}
+        return {self.pragbot_proxy}
 
     def _close(self):
         """Shut down the ROS node."""
