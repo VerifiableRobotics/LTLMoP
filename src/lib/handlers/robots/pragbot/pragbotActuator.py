00b836f3a4dab1d0c8abf268278a08e7dcda68be
Constantine Lignos <lignos@seas.upenn.edu>
2013-11-04 13:03:18 -0500

    Remove cut/paste code and add TODOs for pragbotActuator.py.

--- src/lib/handlers/robots/pragbot/pragbotActuator.py
+++ src/lib/handlers/robots/pragbot/pragbotActuator.py
@@ -1,11 +1,27 @@
-"""LTLMoP motion handler for the JR platform."""
+"""
+LTLMoP motion handler for the pragbot game.
+
+"""
+
+# Copyright (C) 2013 Constantine Lignos
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import time
 
 # Time it takes to defuse a bomb, in seconds
 DEFUSE_TIME = 5.0
-# Wall distance for sweep
-SWEEP_WALL_DISTANCE = 1.3
 
 
 class gumboActuatorHandler(object):
@@ -15,102 +31,49 @@ class gumboActuatorHandler(object):
         self._name = type(self).__name__
         self._sensor_handler = proj.h_instance['sensor'][proj.currentConfig.main_robot]
         self._pose_handler = proj.h_instance['pose']
-
-        # Sweep
-        self._sweep_client = None
-        self._sweep_goal = None
-
-        # Defuse
-        self._defuse_client = None
-        self._defuse_goal = None
-
         self.executor = proj.executor
-
-    def sweep(self, actuatorVal, initial=False):
-        """Perform a search of the current room."""
-        # Create action client
-        if initial:
-            print "Sweep Area Action"
-            return True
-
-        # Activate or deactivate sweep
-        actuatorVal = _normalize(actuatorVal)
-        if actuatorVal:
-            print "{}: Activating sweep.".format(self._name)
-            print "Searching the {}...".format(self._pose_handler.get_location())
-            self._sweep_goal.timeout = 0.0  # Never timeout
-            self._sweep_goal.wall_dist = SWEEP_WALL_DISTANCE
-
-            # Set up a lexically enclosed callback for setting the _done sensor
-            def _complete_sweep(goal_status, goal_result):  # pylint: disable=W0613
-                """Set sweep_done sensor when sweep is complete."""
-                if goal_status is True:
-                    print "{}: Sweep succeeded.".format(self._name)
-                    print "Search Complete"
-                    self._sensor_handler.set_action_done("sweep", True)
-                elif goal_status is False:
-                    print "{}: Sweep cancelled before completion.".format(self._name)
-                else:
-                    print "{}: Sweep failed with status {!r}.".format(self._name, goal_status)
-
-            self._sweep_client.send_goal(self._sweep_goal, done_cb=_complete_sweep)
-            return True
-        else:
-            print "{}: Deactivating sweep.".format(self._name)
-            if self._sweep_goal:
-                self._sweep_client.cancel_goal()
-                self._sweep_goal = None
-
-            # Reset the sweep_done sensor
-            self._sensor_handler.set_action_done("sweep", False)
-            return True
 
     def defuse(self, actuatorVal, initial=False):
         """Defuse a bomb by driving to it and making it disappear."""
-        # Create action client
+        # Perform any initialization required
         if initial:
-            print "Drive to..."
             return True
 
         # Activate or deactivate defuse
         actuatorVal = _normalize(actuatorVal)
         if actuatorVal:
-            # Get the bomb from the sensors
-            bomb = self._sensor_handler.get_sensed_item("bomb")
-            if not bomb:
-                print "{}: Defuse requested but no bomb found.".format(self._name)
-                return False
+            # Update the status that we're defusing.
+            self.executor.postEvent("MESSAGE", "Defuse activated")
+            # TODO: Implement
+            # Get the position of the bomb in the room
 
-            # Move the robot to the bomb
-            bomb_position = bomb.pose.position
+            # If there's no bomb in the room, print an error and
+            # return False.  The return value of this function is not
+            # checked by fsa.py at the moment, so the return value is
+            # never read, but we use it anyway.
 
+            # Update the status that we've defusing.
             self.executor.postEvent("MESSAGE", "Defusing...")
 
-            # Sneakily define a lexically enclosed callback
-            def _complete_defuse(goal_status, goal_result):  # pylint: disable=W0613
-                """Wait until bomb is defused and then remove it from the sensors."""
-                if goal_status is True:
-                    # After the robot reaches the bomb, defuse the
-                    # bomb by waiting and then make it go away.
-                    print "{}: Defusing bomb...".format(self._name)
-                    time.sleep(DEFUSE_TIME)
-                    print "{}: Bomb defusing complete.".format(self._name)
-                    self.executor.postEvent("MESSAGE", "Successfully defused.")
-                    self._sensor_handler.disable_item(bomb)
-                elif goal_status is False:
-                    print "{}: Defuse cancelled before completion.".format(self._name)
-                else:
-                    print "{}: Defuse failed with status {!r}.".format(self._name, goal_status)
+            # Issue a non-blocking command to move the robot to the
+            # bomb position.
 
-            self._defuse_client.send_goal(self._defuse_goal, done_cb=_complete_defuse)
-            print "{}: Moving to bomb at ({}, {}).".format(self._name, bomb_position.x,
-                                                           bomb_position.y)
+            # Define up a lexically-enclosed function that will set the
+            # 'defuse_done' sensor  and make the bomb disappear.
+            def _complete_defuse():  # pylint: disable=W0613
+                """Set defuse_done and remove the bomb from the sensors."""
+                # TODO: Implement. You can set the sensor by using
+                # self._sensor handler, but we'll need to figure out
+                # how to make it disappear in pragbot. Take care of
+                # the sensor_handler for now, and we'll figure out how
+                # to send the client a message later.
+
+            # Use a Timer object to call it after DEFUSE_TIME seconds.
+
             return True
         else:
-            print "{}: Deactivating defuse.".format(self._name)
-            if self._defuse_goal:
-                self._defuse_client.cancel_goal()
-                self._defuse_goal = None
+            # Update the status that we're no longer defusing.
+            self.executor.postEvent("MESSAGE", "Defuse deactivated")
             return True
 
 
