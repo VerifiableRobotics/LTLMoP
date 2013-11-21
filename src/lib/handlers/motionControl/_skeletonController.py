860bf5098a07ec1931cbbccda459c8457faac069
Cameron Finucane <cpf37@cornell.edu>
2013-11-21 11:19:25 -0500

    fix some old bugs in _skeletonMotionController

--- src/lib/handlers/motionControl/_skeletonController.py
+++ src/lib/handlers/motionControl/_skeletonController.py
@@ -25,7 +25,7 @@ class motionControlHandler:
         if current_reg == next_reg and not last:
             # No need to move!
             self.drive_handler.setVelocity(0, 0)  # So let's stop
-            return False   # Special case
+            return True  # Already there
 
         # Find our current configuration
         pose = self.pose_handler.getPose()
@@ -37,10 +37,11 @@ class motionControlHandler:
 
         # TODO: Calculate a velocity vector in the *GLOBAL REFERENCE FRAME* 
         # that will get us on our way to the next region
-        [vx, vy, w] = [0, 0, 0]
+        vx, vy = 0, 0
 
         # Pass this desired velocity on to the drive handler
-        self.drive_handler.setVelocity(vx, vy, w)
+        # It also gets the current heading of the robot
+        self.drive_handler.setVelocity(vx, vy, pose[2])
         
         # Figure out whether we've exited the current region
         if is_inside([pose[0], pose[1]], vertices):
