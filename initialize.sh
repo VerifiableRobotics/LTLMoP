e68dd30dc586f80258d0e80f2690efdefdc72073
Anton Gilgur <agilgur5@gmail.com>
2015-11-21 01:29:54 -0500

    (env): add GUI image default command specEditor
    - remove Vagrantfile in preference for sheer Docker moving forward

--- initialize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# install docker
-wget -q -O - https://get.docker.io/gpg | apt-key add -;
-echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list;
-apt-get update -qq;
-apt-get install -q -y --force-yes lxc-docker;
-# add vagrant user to the docker group
-usermod -a -G docker vagrant;
